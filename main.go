de195053fe71f2dfe6c4e51356612313c7967498
Luiz Filipe S Mariz <48025000+lfsmariz@users.noreply.github.com>
2022-05-03 20:30:30 -0300

    Feature/chuva ou sol (#2)
    * criada função para retornar chuva ou sol
    
    * criada função para randomizar o tempo

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+
+	fmt.Println(chuvasol(randomSun()))
+
+}
+
+func chuvasol(b bool) string {
+	if b {
+		return "chuva"
+	}
+
+	return "sol"
+}
+
+func randomSun() bool {
+	i := rand.Intn(100)
+
+	if i > 50 {
+		return true
+	}
+
+	return false
 }
